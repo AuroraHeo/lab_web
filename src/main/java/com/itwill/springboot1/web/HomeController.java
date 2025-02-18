fc98dcc9f1c8372667cb7d4a844bee30d671ffbf
yujinHeo <youjin2586@gmail.com>
2025-02-18 17:44:29 +0900

    Initial commit for springboot1 project

---
+++ src/main/java/com/itwill/springboot1/web/HomeController.java
@@ -0,0 +1,96 @@
+package com.itwill.springboot1.web;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itwill.springboot1.domain.Author;
+import com.itwill.springboot1.domain.Book;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		log.info("home() 호출");
+		
+		LocalDateTime now = LocalDateTime.now(); //서버 현재 시간
+		model.addAttribute("currentTime", now); //뷰에 전달되는 데이터
+		
+		Author author = Author.builder()
+				.firstName("강").lastName("한").build();
+		
+		Book book = Book.builder()
+				.id(100100).title("채식주의자").author(author).build();
+		model.addAttribute("book", book);
+		
+		return "index";
+		//컨트롤러 메서드의 리턴 값(문자열) -> 뷰의 이름
+		//스프링 부트 프로젝트에서는 src/main/resources/templates/리턴값.html
+	}
+	
+	@GetMapping("/book/list")
+	public void bookList(Model model) {
+		log.info("bookList()");
+		
+		//도서 목록을 저장할 ArrayList:
+		ArrayList<Book> list = new ArrayList<>();
+		
+		//list에 Book타입의 객체를 5개를 저장
+		for(int i = 0; i < 5; i++) {
+			Author author = Author.builder().firstName("FirstName_" + i).lastName("LastName_" + i).build();
+			
+			Book book = Book.builder().id(i).title("Title_" + i).author(author).build();
+			
+			list.add(book); //책 5개가 됨
+		}
+		
+		// author가 null인 book 객체를 리스트에 추가.
+		list.add(Book.builder().id(123).title("No Title").build()); //총 6개 
+		
+//		list.add(Book.builder().id(12345).title("제목1").author(Author.builder().firstName("유진").lastName("허").build()).build());
+//		list.add(Book.builder().id(21212).title("제목2").author(Author.builder().firstName("유진").lastName("허").build()).build());
+//		list.add(Book.builder().id(35255).title("제목3").author(Author.builder().firstName("유진").lastName("허").build()).build());
+//		list.add(Book.builder().id(47973).title("제목4").author(Author.builder().firstName("유진").lastName("허").build()).build());
+//		list.add(Book.builder().id(50505).title("제목5").author(Author.builder().firstName("유진").lastName("허").build()).build());
+		
+		//list를 뷰에 전달.
+		model.addAttribute("bookList", list);
+		
+		//뷰(html) 작성.
+		// -> 컨트롤러 메서드가 void인 경우 뷰의 이름은 요청 URL과 같음.
+	}
+	
+	@GetMapping("/book/details")
+	public void details(Integer id, Model model) {
+		//이클립스 메뉴 > Window > Preferences > java > compiler > store information about parameter~ 항목이 체크되어 있으면
+		// @RequestParam 생략할 수 있음.
+		log.info("details(id={})", id);
+		
+		Book book = Book.builder().id(id).title("무제").build();
+		model.addAttribute("book", book);
+		
+	}
+	
+	@GetMapping("/book/details/{id}")
+	public String bookDetails(@PathVariable Integer id, Model model) {
+		//이클립스 메뉴 > Window > Preferences > java > compiler > store information about parameter~ 항목이 체크되어 있으면
+		// @PathVariable에서 name 속성을 생략할 수 있음 ex)@PathVariable(name="id")을 간단히 @PathVariable로 사용 가능.
+		log.info("bookDetails(id={})",id);
+		
+		Book book = Book.builder().id(id).title("채식주의자").build();
+		model.addAttribute("book", book);
+		
+		return "book/details";
+		//-> src/main/resources/templates/book/details.html
+	}
+
+}
